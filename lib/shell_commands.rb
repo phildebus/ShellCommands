69e04433d669a8330ad30d1bcfbc3ac239d7026d
debus.phil <debus.phil@gmail.com>
2013-03-08 15:47:26 -0800

    Change command results to use symbols instead of strings. Add get_result_of command.

--- lib/shell_commands.rb
+++ lib/shell_commands.rb
@@ -5,12 +5,17 @@ def do_command(command)
 	ShellCommand.do_command(command)
 end
 
+def get_result_of(command)
+  do_command command
+  ShellCommand.last_result[:result]
+end
+
 def result_matches(regex)
-	return !regex.match(ShellCommand.last_result["result"]).nil? if regex.class == "Regexp"
-	return !Regexp.new(regex).match(ShellCommand.last_result["result"]).nil?
+	return !regex.match(ShellCommand.last_result[:result]).nil? if regex.class == "Regexp"
+	return !Regexp.new(regex).match(ShellCommand.last_result[:result]).nil?
 end
 
 def regex_result(regex)
-  return regex.match(ShellCommand.last_result["result"]) if regex.class == "Regexp"
-  return Regexp.new(regex).match(ShellCommand.last_result["result"])
+  return regex.match(ShellCommand.last_result[:result]) if regex.class == "Regexp"
+  return Regexp.new(regex).match(ShellCommand.last_result[:result])
 end
